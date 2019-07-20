ca37a4321a5927cb0131fb10e4d183108d50bef9
Carlos Garces <carlos.garces@gmail.com>
2019-07-20 11:37:43 +0200

    first commit

---
+++ packages/linux-driver-addons/wifi/RTL8189ES/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8189ES"
+PKG_VERSION="6279391"
+PKG_SHA256="8e1b90ccd232f90bd1dfa5e31d057268ea46ed00274a413966db9e1f1245c18d"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/jwrdegoede/rtl8189ES_linux/"
+PKG_URL="https://github.com/jwrdegoede/rtl8189ES_linux/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_SECTION="driver.wifi"
+PKG_LONGDESC="Realtek RTL8189ES Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+PKG_IS_ADDON="embedded"
+PKG_ADDON_IS_STANDALONE="yes"
+PKG_ADDON_NAME="RTL8189ES wifi drivers"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+
+  make ARCH=$TARGET_KERNEL_ARCH \
+    KSRC=$(kernel_path) \
+    CROSS_COMPILE=$TARGET_KERNEL_PREFIX 
+}
+
+makeinstall_target() {
+  install_driver_addon_files "$PKG_BUILD/"
+}
---
+++ packages/linux-driver-addons/wifi/RTL8189ES/source/default.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Carlos Garces CGarces@users.noreply.github.com
